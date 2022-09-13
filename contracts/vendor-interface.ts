8bbc5617666405f1c0d95ce6abfbb7ac68d20147
yudianwebdev <69705828+yudianwebdev@users.noreply.github.com>
2022-09-13 16:16:27 +0700

    feat(vendor): implementasi api add vendor (#20)

---
+++ contracts/vendor-interface.ts
@@ -0,0 +1,11 @@
+export interface IVendor {
+  ID: number;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  type: string;
+  url_website: string;
+  country_id: number;
+  description: string;
+}
--- pages/vendor/add.tsx
+++ pages/vendor/add.tsx
@@ -1,6 +1,12 @@
+import { fetcher } from '@api/fetcher';
 import HeadingTop from '@components/TopComponents/Heading';
+import { IProvince } from '@contracts/client-interface';
+import { IVendor } from '@contracts/vendor-interface';
+import useInput from '@hooks/useInput';
 import { Button, createStyles, Grid, Select, Text, Textarea, TextInput } from '@mantine/core';
 import { IconChevronDown } from '@tabler/icons';
+import { useTransition } from 'react';
+import useSWR from 'swr';
 
 const useStyles = createStyles(() => ({
   label: {
@@ -18,14 +24,43 @@ const useStyles = createStyles(() => ({
 }));
 function VendorPage() {
   const { classes } = useStyles();
+  const [, startTransition] = useTransition();
+
+  const [input, handleInput] = useInput({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    type: '',
+    url_website: '',
+    country_id: '',
+    description: '',
+  });
+  const addData = async () => {
+    const response: IVendor | undefined = await fetcher('/api/v1/vendors/', {
+      method: 'POST',
+      body: {
+        name: input.name,
+        email: input.email,
+        phone: input.phone,
+        address: input.address,
+        type: input.type,
+        url_website: input.url_website,
+        country_id: Number(input.country_id),
+        description: input.description,
+      },
+    });
+    // eslint-disable-next-line no-console
+    console.log('Response from API ', response);
+  };
+  const { data: country } = useSWR<IProvince[]>('/api/v1/countries/');
   return (
     <>
       <HeadingTop
-        text="Vendor"
+        text="Add New Vendor"
         items={[
-          { title: 'Mantine', href: '#' },
-          { title: 'Mantine hooks', href: '#' },
-          { title: 'use-id', href: '#' },
+          { title: 'Vendor', href: '/vendor' },
+          { title: 'Add New Vendor', href: '' },
         ]}
       />
       <div className="mx-5">
@@ -35,36 +70,84 @@ function VendorPage() {
 
         <Grid gutter="xl" className="mb-[48px]">
           <Grid.Col md={6}>
-            <TextInput label="Name" placeholder="e.g Herjanto" />
+            <TextInput label="Name" placeholder="e.g Herjanto" value={input.name} onChange={handleInput('name')} />
           </Grid.Col>
 
-          <Grid.Col md={6}>
-            <Select label="Type" placeholder="Select Type" rightSection={<IconChevronDown size={14} />} data={[]} />
-          </Grid.Col>
-
-          <Grid.Col md={6}>
-            <TextInput label="Website" placeholder="e.g www.tokopedia.com/erajayabubut" />
-          </Grid.Col>
-          <Grid.Col md={6}>
-            <TextInput label="Phone Number" placeholder="e.g 0837xxxxxxxx" />
-          </Grid.Col>
-
-          <Grid.Col md={6}>
-            <Select label="City" placeholder="Select City" rightSection={<IconChevronDown size={14} />} data={[]} />
-          </Grid.Col>
           <Grid.Col md={6}>
             <Select
-              label="Province"
-              placeholder="Select Province"
+              label="Type"
+              placeholder="Select Type"
               rightSection={<IconChevronDown size={14} />}
-              data={[]}
+              data={[
+                { value: 'type 1', label: 'Type 1' },
+                { value: 'type 2', label: 'Type 2' },
+                { value: 'type 3', label: 'Type 3' },
+              ]}
+              onChange={(v) => {
+                startTransition(() => {
+                  handleInput('type', true)(v);
+                });
+              }}
+              value={input.type}
             />
           </Grid.Col>
+
+          <Grid.Col md={6}>
+            <TextInput
+              label="Website"
+              value={input.url_website}
+              onChange={handleInput('url_website')}
+              placeholder="e.g www.tokopedia.com/erajayabubut"
+            />
+          </Grid.Col>
+
+          <Grid.Col md={6}>
+            <Select
+              label="Country"
+              placeholder="Select Country"
+              rightSection={<IconChevronDown size={14} />}
+              onChange={(v) => {
+                startTransition(() => {
+                  handleInput('country_id', true)(v);
+                });
+              }}
+              value={input.country_id}
+              data={country ? country.map((y) => ({ value: y.ID.toString(), label: y.name })) : []}
+            />
+          </Grid.Col>
+          <Grid.Col md={6}>
+            <TextInput
+              label="Address"
+              value={input.address}
+              onChange={handleInput('address')}
+              placeholder="e.g jl. k.h. agus salim no.07"
+            />
+          </Grid.Col>
+          <Grid.Col md={6}>
+            <TextInput
+              label="Email"
+              value={input.email}
+              onChange={handleInput('email')}
+              placeholder="e.g erajayabubut@gmail.com"
+            />
+          </Grid.Col>
+          <Grid.Col md={6}>
+            <TextInput
+              label="Phone Number"
+              value={input.phone}
+              onChange={handleInput('phone')}
+              placeholder="e.g 0837xxxxxxxx"
+            />
+          </Grid.Col>
+
+          <Grid.Col md={6} />
           <Grid.Col md={12}>
             <Textarea
               styles={{ input: { height: 'unset !important' } }}
               className={classes.label}
               label="Description"
+              value={input.description}
+              onChange={handleInput('description')}
               placeholder="Description"
               minRows={4}
             />
@@ -74,7 +157,9 @@ function VendorPage() {
             <Button className={`${classes.cancel} hover:bg-transparent`}>Cancel</Button>
           </Grid.Col>
           <Grid.Col md={2}>
-            <Button className={`${classes.root} bg-black hover:bg-black`}>Save</Button>
+            <Button className={`${classes.root} bg-black hover:bg-black`} onClick={() => addData()}>
+              Save
+            </Button>
           </Grid.Col>
         </Grid>
       </div>
